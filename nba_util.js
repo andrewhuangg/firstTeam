9532a0552e48dadedc300abe485889859f1b8227
Andrew <drewhuangg@gmail.com>
2019-09-30 16:11:19 -0700

    added api endpoints

---
+++ nba_util.js
@@ -0,0 +1,71 @@
+export const getAllStats = () => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: "https://www.balldontlie.io/api/v1/stats"
+    })
+  );
+};
+
+export const getAllAvgs = () => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: "https://www.balldontlie.io/api/v1/season_averages"
+    })
+  )
+}
+
+export const getAllPlayers = () => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: "https://www.balldontlie.io/api/v1/players"
+    })
+  );
+};
+
+export const getPlayer = (id) => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: `https://www.balldontlie.io/api/v1/players/${id}`
+    })
+  );
+};
+
+export const getAllTeams = () => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: "https://www.balldontlie.io/api/v1/teams"
+    })
+  )
+}
+
+export const getTeam = (id) => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: `https://www.balldontlie.io/api/v1/teams/${id}`
+    })
+  );
+};
+
+export const getAllGames = () => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: "https://www.balldontlie.io/api/v1/games"
+    })
+  )
+}
+
+export const getGame = (id) => {
+  return (
+    $.ajax({
+      method: "GET",
+      url: `https://www.balldontlie.io/api/v1/games/${id}`
+    })
+  );
+};
