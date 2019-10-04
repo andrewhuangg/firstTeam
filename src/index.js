34251be7b9746006e7e083ad6f2ea79a1f855035
Andrew <drewhuangg@gmail.com>
2019-10-04 11:47:59 -0700

    updated event handler

--- src/index.js
+++ src/index.js
@@ -6,22 +6,25 @@ import {
   scaleBand,
   axisLeft,
   axisTop,
+  axisBottom,
   extent,
   bandwidth,
   zoom,
   event
 } from 'd3';
-import { loadData } from './loadData'
+
+import { 
+  loadAvg,
+  loadPer100
+} from './loadData';
 
 const svg = select('svg')
-const width = 300; //document.body.clientWidth;
-const height = 600;
+const width = 600; //document.body.clientWidth;
+const height = 700;
 
 svg
   .attr('width', width)
   .attr('height', height)
-
-
 /*
 .data(data) is a data join with three cases, 'enter', 'update', 'exit' 
 in which case data / elements, data is our array of data entries, elements are
@@ -32,9 +35,44 @@ the dom elements, (the rectangles)
     the enter selection creates a thing that pertains to all our data elements
     and we use this to create a rect for row of our table
 */
-loadData().then(data => {
+
+let playerData = {
+  'Pos': 'Player Position',
+  'AST': 0,
+  'BLK': 0,
+  'STL': 0,
+  'FG': 0,
+  'TRB': 0,
+  'FT': 0,
+};
+
+loadAvg(playerData).then(data => {
   render(data);
 });
+
+const handlePosChange = (e) => {
+  playerData.Pos = e.target.value;
+  d3.selectAll('svg > *').remove();
+  loadAvg(playerData).then(data => {
+    render(data);
+  });
+};
+
+let stat;
+
+const handleStatChange = (e) => {
+  playerData[stat] = e.target.value;
+  d3.selectAll('svg > *').remove();
+  loadAvg(playerData).then(data => {
+    render(data);
+  });
+};
+
+document.getElementById('posOptions')
+  .addEventListener('change', handlePosChange)
+
+document.getElementById('statOptions')
+  .addEventListener('change', handleStatChange)
 
 
 const render = (data) => {
@@ -46,8 +84,9 @@ const render = (data) => {
 
   const xScale = scaleLinear()
     .domain([0, max(data, xValue)])
-    .range([0, innerWidth]) //an instance of scaleLinear which maps the numbers from the domain to the range(it scales it), domain(the data space, the min and max values of our data), range(the screen space, typically in px coordinates, the width of each bar in pixels)
-  
+    .range([0, innerWidth])
+    .nice() //an instance of scaleLinear which maps the numbers from the domain to the range(it scales it), domain(the data space, the min and max values of our data), range(the screen space, typically in px coordinates, the width of each bar in pixels)
+
   const yScale = scaleBand()
     .domain(data.map(yValue))
     .range([0, innerHeight])
@@ -72,8 +111,8 @@ const render = (data) => {
   //xAxis title
   xAxisG.append('text').text('Points')
     .attr('class', 'xLabel')
-    .attr('transform', `translate(${margin.left - 100})`)
-    .attr('y', -25)
+    .attr('transform', `translate(${width / 2 - 100})`)
+    .attr('y', -30)
     .attr('fill', 'black')
     
   xAxisG.select('.domain').remove();
@@ -97,16 +136,16 @@ const render = (data) => {
       .attr('text-anchor', 'middle')
       .text(d => `${xValue(d)}`)
     .append('title')
-    .text(d => d.PTS)
+    .text(d => d.Tm)
     
 
   //svg header
   svg.append('text')
     .attr('class', 'title')
-    .text('2018 PGs Avgs +10Pts +10MP')
-    .attr('transform', `translate(${margin.left / 2}, ${margin.top - 50})`)
+    .text(`2018 ${playerData.Pos} Avgs +10Pts +10MP`)
+    .attr('transform', `translate(${width / 2 - 100}, ${margin.top - 50})`)
 
-    //zoom
+  //   zoom
   // svg.call(zoom().on('zoom', () => {
   //   g.attr('transform', event.transform);
   // }));
--- src/loadData.js
+++ src/loadData.js
@@ -6,53 +6,46 @@ import {
 let val = null;
 
 
-export const loadData = () => 
+export const loadAvg = (playerData) => 
   Promise.all([
     csv('../data/playerSeasonAvg.csv')  
   ]).then(([seasonAvgData]) => {
 
-    let pos = 'PG';
-
-    const handleChange = (e) => {
-      e.preventDefault();
-      seasonAvgData.forEach(d => {
-        if (d.Pos === e.target.value) {
-          pos = d.Pos
-          return pos;
-        }
-      });
-    };
-
-    const val = document.getElementById('options')
-      .addEventListener('change', handleChange)
-
-    
     const filtered = seasonAvgData
       .sort((a, b) => b.PTS - a.PTS)
-      .filter(d => d.Tm !== 'TOT' && d.PTS > 10 && d.MP > 10 && d.Pos === pos)
+      .filter(d => d.Tm !== 'TOT' && d.PTS > 10 && d.MP > 10 && d.Pos === playerData.Pos)
 
     filtered
       .forEach(d => {
         d.Player = d.Player;
         d.PTS = +d.PTS;
+        d.Tm = d.Tm;
         // d.Age;
         // d.G;
         // d.MP;
-        // d.FG;
+        d.FG = +d.FG;
         // d.FGpct;
         // d.threeP;
         // d.threePpct;
         // d.twoP;
         // d.twoPpct;
         // d.eFGpct;
-        // d.FT;
+        d.FT = +d.FT;
         // d.FTpct;
-        // d.TRB;
-        // d.AST;
-        // d.STL;
-        // d.BLK;
+        d.TRB = +d.TRB;
+        d.AST = +d.AST;
+        d.STL = +d.STL;
+        d.BLK = +d.BLK;
         // d.TOV;
         // d.PF;
       });
     return filtered;
+  });
+
+export const loadPer100 = (pos) =>
+  Promise.all([
+    csv('../data/playerPer100Pos.csv')
+  ]).then(([per100PosData]) => {
+
+
   });
